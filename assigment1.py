3767ea783cbf4a2d19e86a1b82eaa361882d600d
varakarapureddy <99267924+varakarapureddy@users.noreply.github.com>
2022-02-25 20:40:50 +0530

    done  assigment 1

---
+++ assigment1.py
@@ -0,0 +1,8 @@
+n=int(input("enter number of numbers"))
+numbers=[]
+for i in range(n):
+    temp=int(input())
+    numbers.append
+    numbers.append(temp)
+numbers.sort(reverse=True)    
+print(numbers)
