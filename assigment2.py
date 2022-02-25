85197903b6a21abe2f0e2134b67abdfb4445256d
varakarapureddy <99267924+varakarapureddy@users.noreply.github.com>
2022-02-25 20:42:19 +0530

    done assigment 2

---
+++ assigment2.py
@@ -0,0 +1,13 @@
+n=int(input("enter number of numbers"))
+numbers=[]
+output=[]
+for i in range(n):
+    temp=int(input())
+    numbers.append
+    numbers.append(temp)
+for i in numbers:
+    if i != 0:
+        output.append(i)
+    
+print(output)
+print(len(output))
